523d1394935d6a844fbfa730451cc9c8f57b0743
Monique Silva <moniquesntsilva@gmail.com>
2021-12-21 13:50:13 -0300

    feat: normalize css

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,9 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        additionalData: `@import "@/styles/variables.scss";`,
+        additionalData: `@import "@/styles/variables.scss";
+        @import "@/styles/normalize.scss";
+        `,
       },
     },
   },
