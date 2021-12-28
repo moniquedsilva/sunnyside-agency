0ea4fb98e6e365317e165f0560a407c8c604dd22
Monique Silva <moniquesntsilva@gmail.com>
2021-12-28 19:54:55 -0300

    feat: complete about section

--- src/components/About/index.vue
+++ src/components/About/index.vue
@@ -9,7 +9,16 @@
         {{ item.title }}
       </h2>
       <p class="about__paragraph">{{ item.text }}</p>
-      <a href="#">Learn More</a>
+      <a
+        href="#"
+        :class="
+          item.id == 1
+            ? ' about__links about__linkLeft'
+            : 'about__links about__linkRight'
+        "
+      >
+        <span>Learn More</span></a
+      >
     </section>
     <div role="image" class="about__bgRight"></div>
     <div role="image" class="about__bgLeft"></div>
@@ -41,6 +50,7 @@ export default {
     grid-area: left;
     display: flex;
     flex-direction: column;
+    align-items: center;
     justify-content: center;
     gap: 2rem;
     width: 70%;
@@ -51,6 +61,7 @@ export default {
     grid-area: right;
     display: flex;
     flex-direction: column;
+    align-items: center;
     justify-content: center;
     gap: 2rem;
     width: 70%;
@@ -61,11 +72,52 @@ export default {
     font: 1.75rem $fraunces;
     color: $dark-desaturated-blue;
     margin: 0;
+    font-weight: 900;
   }
   &__paragraph {
     font: 0.875rem $barlow;
     color: $dark-gray-blue;
     line-height: 1.7;
+  }
+  &__links {
+    position: relative;
+    width: max-content;
+    padding: 1rem 0.375rem;
+    color: $dark-desaturated-blue;
+    font: 1rem $fraunces;
+    font-weight: 900;
+    letter-spacing: 0.0625rem;
+    text-decoration: none;
+    text-transform: uppercase;
+    &::after {
+      border-radius: 5px;
+      bottom: 0.875rem;
+      content: '';
+      display: block;
+      height: 0.625rem;
+      left: 0;
+      opacity: 0.3;
+      position: absolute;
+      right: 0;
+      transition: opacity 0.3s ease-in-out;
+    }
+    &:hover,
+    &:active {
+      color: #000;
+      &::after {
+        opacity: 1;
+      }
+    }
+    span {
+      position: relative;
+      z-index: 8;
+    }
+  }
+  &__linkLeft::after {
+    background-color: $yellow;
+  }
+  &__linkRight::after {
+    background-color: $soft-red;
   }
   &__bgRight {
     background: url('../../../public/assets/images/mobile/image-transform.jpg')
@@ -87,12 +139,13 @@ export default {
       'left imgRight'
       'imgLeft right';
     &__left {
+      align-items: flex-start;
       gap: 2rem;
-      width: 28rem;
       text-align: left;
     }
     &__right {
-      width: 28rem;
+      align-items: flex-start;
+      gap: 2rem;
       text-align: left;
     }
     &__title {
@@ -108,6 +161,20 @@ export default {
 @media (min-width: 64em) {
   .about {
     grid-template-rows: repeat(2, minmax(585px, 1fr));
+    &__left {
+      width: 28rem;
+    }
+    &__right {
+      width: 28rem;
+    }
+    &__bgRight {
+      background: url('../../../public/assets/images/desktop/image-transform.jpg')
+        center/cover no-repeat hsla(50, 99%, 50%, 1);
+    }
+    &__bgLeft {
+      background: url('../../../public/assets/images/desktop/image-stand-out.jpg')
+        center/cover no-repeat hsla(7, 99%, 69%, 1);
+    }
   }
 }
 
