116b066b6e30c9db6d7899a488ef82242deec0fc
Monique Silva <moniquesntsilva@gmail.com>
2021-12-28 11:15:46 -0300

    feat: initial structure

---
+++ src/components/About/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="about" class="about">
+    <section
+      v-for="item in info"
+      :key="item.id"
+      :class="item.id == 1 ? 'about__left' : 'about__right'"
+    >
+      <h2>
+        {{ item.title }}
+      </h2>
+      <p>{{ item.text }}</p>
+      <a href="#">Learn More</a>
+    </section>
+    <div role="image" class="about__bgRight"></div>
+    <div role="image" class="about__bgLeft"></div>
+  </div>
+</template>
+
+<script>
+import { information } from './information'
+
+export default {
+  data() {
+    return {
+      info: information,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.about {
+  display: grid;
+  grid-template-rows: repeat(4, minmax(292px, 1fr));
+  grid-template-areas:
+    'imgRight'
+    'left'
+    'imgLeft'
+    'right';
+  &__leftSection {
+    grid-area: left;
+  }
+  &__rightSection {
+    grid-area: right;
+  }
+  &__bgRight {
+    background: url('../../../public/assets/images/mobile/image-transform.jpg')
+      center/cover no-repeat hsla(50, 99%, 50%, 1);
+    grid-area: imgRight;
+  }
+  &__bgLeft {
+    background: url('../../../public/assets/images/mobile/image-stand-out.jpg')
+      center/cover no-repeat hsla(7, 99%, 69%, 1);
+    grid-area: imgLeft;
+  }
+}
+
+@media (min-width: 48em) {
+  .about {
+    grid-template-rows: repeat(2, minmax(292px, 1fr));
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-template-areas:
+      'left imgRight'
+      'imgLeft right';
+  }
+}
+
+@media (min-width: 64em) {
+  .about {
+    grid-template-rows: repeat(2, minmax(585px, 1fr));
+  }
+}
+</style>
---
+++ src/components/About/information.js
@@ -0,0 +1,14 @@
+export const information = [
+  {
+    id: 1,
+    title: 'Transform your brand',
+    text: 'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.',
+    link: 'learn more',
+  },
+  {
+    id: 2,
+    title: 'Stand out to the right audience',
+    text: 'Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.',
+    link: 'learn more',
+  },
+]
