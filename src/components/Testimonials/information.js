fae7a5cc9306f8dddf5cff869fd7c53bf8e2516d
Monique Silva <moniquesntsilva@gmail.com>
2021-12-29 11:05:04 -0300

    feat: clients data

---
+++ src/components/Testimonials/information.js
@@ -0,0 +1,23 @@
+export const information = [
+  {
+    id: 1,
+    img: 'assets/images/image-emily.jpg',
+    text: 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.',
+    name: 'Emily R.',
+    profession: 'Marketing Director',
+  },
+  {
+    id: 2,
+    img: 'assets/images/image-thomas.jpg',
+    text: "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.",
+    name: 'Thomas S.',
+    profession: 'Chief Operating Officer',
+  },
+  {
+    id: 3,
+    img: 'assets/images/image-jennie.jpg',
+    text: 'Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!',
+    name: 'Jennie F.',
+    profession: 'Business Owner',
+  },
+]
